2ec6a973f38dfeec0bb18e76f8752476c57644c8
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-21 05:23:49 +0530

    Update gitupload.py

--- userbot/modules/gitupload.py
+++ userbot/modules/gitupload.py
@@ -18,7 +18,6 @@ from userbot import CMD_HELP, GITHUB_ACCESS_TOKEN, GIT_REPO_NAME, bot
 
 GIT_TEMP_DIR = "./userbot/temp/"
 # @borg.on(admin_cmd(pattern="commit ?(.*)", allow_sudo=True))
-
 @register(outgoing=True, pattern="^.commt(?: |$)(.*)")
 async def download(event):
     if event.fwd_from:
