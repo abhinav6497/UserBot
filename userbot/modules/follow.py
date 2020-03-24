4a401ba407695f2bfee2b646d925c766ee3afe18
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-24 22:03:11 +0530

    Update follow.py

--- userbot/modules/follow.py
+++ userbot/modules/follow.py
@@ -25,7 +25,7 @@ async def follow(follow):
     """ For .follow command, check if the bot is running.  """
     await follow.edit(
                      f"`FOLLOW {DEFAULTUSER} ON` \n"
-                     f"InstaGram : https://www.instagram.com/mayur_karaniya \n"
-                     f"FaceBook : https://www.facebook.com/mkaraniya \n"
-                     f"YouTube : https://www.youtube.com/channel/UCeKQxQK7XZ3jGi3541uWATg?sub_confirmation=1 "
+                     f"[InstaGram](https://www.instagram.com/mayur_karaniya) \n"
+                     f"[FaceBook](https://www.facebook.com/mkaraniya) \n"
+                     f"[YouTube](https://www.youtube.com/channel/UCeKQxQK7XZ3jGi3541uWATg?sub_confirmation=1) "
                      )    
