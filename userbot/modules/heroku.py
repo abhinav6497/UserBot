d43fc85b1ded6847c21650bd9dbbcc3cf0310d37
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-29 17:23:34 +0530

    Update heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -72,7 +72,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-async def subprocess_run(cmd, heroku):
+async def asyncrunapp_run(cmd, heroku):
     subproc = await asyncrunapp(cmd, stdout=asyncPIPE, stderr=asyncPIPE)
     stdout, stderr = await subproc.communicate()
     exitCode = subproc.returncode
@@ -91,13 +91,13 @@ async def subprocess_run(cmd, heroku):
 #async def _event(heroku):
 #@register(outgoing=True, pattern=r"^!heroku(?: |$)(.*)")
 @register(outgoing=True, pattern="^.heroku$")
-async def heroku_manager(heroku):
-    await heroku.edit("`Processing...`")
+async def heroku_manager(manager):
+    await manager.edit("`Processing...`")
     await asyncio.sleep(3)
-    conf = heroku.pattern_match.group(1)
-    result = await subprocess_run(f'heroku ps -a {HEROKU_APP_NAME}', heroku)
+    conf = manager.pattern_match.group(1)
+    result = await asyncrunapp_run(f'heroku ps -a {HEROKU_APP_NAME}', manager)
     if result[2] != 0:
         return
     hours_remaining = result[0]
-    await heroku.edit('`' + hours_remaining + '`')
+    await manager.edit('`' + hours_remaining + '`')
     return
