cb5cc48b0fd11f74fc7caea9085a4d2138e919fd
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-29 20:11:46 +0530

    Update heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -121,3 +121,47 @@ async def sysdetails(sysd):
         await sysd.edit("`" + result + "`")
     except FileNotFoundError:
         await sysd.edit("`Install neofetch first !!`")
+
+        
+
+@register(outgoing=True, pattern="^.pip(?: |$)(.*)")
+async def pipcheck(pip):
+    """ For .pip command, do a pip search. """
+    pipmodule = pip.pattern_match.group(1)
+    if pipmodule:
+        await pip.edit("`Searching . . .`")
+        invokepip = f"pip3 search {pipmodule}"
+        pipc = await asyncrunapp(
+            invokepip,
+            stdout=asyncPIPE,
+            stderr=asyncPIPE,
+        )
+
+        stdout, stderr = await pipc.communicate()
+        pipout = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+
+        if pipout:
+            if len(pipout) > 4096:
+                await pip.edit("`Output too large, sending as file`")
+                file = open("output.txt", "w+")
+                file.write(pipout)
+                file.close()
+                await pip.client.send_file(
+                    pip.chat_id,
+                    "output.txt",
+                    reply_to=pip.id,
+                )
+                remove("output.txt")
+                return
+            await pip.edit("**Query: **\n`"
+                           f"{invokepip}"
+                           "`\n**Result: **\n`"
+                           f"{pipout}"
+                           "`")
+        else:
+            await pip.edit("**Query: **\n`"
+                           f"{invokepip}"
+                           "`\n**Result: **\n`No Result Returned/False`")
+    else:
+        await pip.edit("`Use .help pip to see an example`")
