32203504d209554e41f1e8d0b3dfb848bf87bab0
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-30 02:57:57 +0530

    Update heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -95,7 +95,7 @@ async def heroku_manager(manager):
     await manager.edit("`Processing...`")
     await asyncio.sleep(3)
     conf = manager.pattern_match.group()
-    result = await asyncrunapp_run(f'manager ps -a {HEROKU_APP_NAME}', manager)
+    result = await asyncrunapp_run(f'heroku ps -a {HEROKU_APP_NAME}', manager)
     if result != 0:
         return
     hours_remaining = result
