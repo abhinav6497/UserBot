d293c078afb50a5434c67c989128dcd40433a17a
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-30 03:53:46 +0530

    Update heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -91,7 +91,7 @@ async def heroku_manager(manager):
 
 
 async def asyncrunapp_run(cmd, heroku):
-    subproc = await cmd(stdout=asyncPIPE, stderr=asyncPIPE)
+    subproc = await heroku(cmd, stdout=asyncPIPE, stderr=asyncPIPE)
     stdout, stderr = await subproc.communicate()
     exitCode = subproc.returncode
     if exitCode != 0:
