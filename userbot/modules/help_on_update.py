3f71f82255f3b95f9229a1a5c7a28bde30fc5472
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-01 20:48:49 +0530

    Create help_on_update.py
    this would be awesome

---
+++ userbot/modules/help_on_update.py
@@ -0,0 +1,34 @@
+""" Userbot module for other small commands. """
+
+from random import randint
+from asyncio import sleep
+from os import execl
+import sys
+import os
+import io
+import sys
+import json
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot, GIT_REPO_NAME, ALIVE_NAME
+from userbot.events import register
+
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+@register(outgoing=True, pattern="^.useit$")
+async def usit(e):
+    await e.edit(
+        "Here's something for you to useit for help_on_update:\n"
+        "\n[Windows Method](https://telegra.ph/How-to-keep-repo-updated-while-keeping-your-changes-through-windows-cmd-method-04-01)"
+        "\n[Termux Method](https://telegra.ph/How-to-keep-OpenUserBot-repo-updated-while-keeping-your-changes-through-Termux-method-04-01)"
+        "\n[Kali Linux Method](https://telegra.ph/How-to-keep-OpenUserBot-repo-updated-while-keeping-your-changes-through-Termux-method-04-01)"
+        "\n[Ubuntu Linux Method](https://telegra.ph/How-to-keep-OUB-repo-updated-while-keeping-your-changes-through-Ubuntu-Terminal-method-04-01-2)"
+        "\n[Special - Note](https://telegra.ph/Special-Note-11-02)")
+    
+    
+CMD_HELP.update({
+    "useit":
+    ".useit\
+\nUsage: Provide links to update repo guides while you keep your changes on the floor."
+})
