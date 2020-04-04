b4d80c2b6d136e2c917ed660f3869b3eb4ee1445
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-04 19:18:32 +0530

    Update admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -2,6 +2,7 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
+# cpin courtesy Uniborg.
 """
 Userbot module to help you manage a group
 """
