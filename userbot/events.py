71513925819d0a0a24393c5f6945ececdc32174b
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-08 01:19:48 +0530

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -135,10 +135,8 @@ def register(**args):
                     file.close()
 
                     if LOGSPAMMER:
-                        await check.client.respond(
-                            "`Sorry, my userbot has crashed.\
-                        \nThe error logs are stored in the userbot's log chat.`"
-                        )
+                        await check.client.respond("`Sorry, my userbot has crashed.\
+                        \nThe error logs are stored in the userbot's log chat.`")
 
                     await check.client.send_file(send_to,
                                                  "error.log",
