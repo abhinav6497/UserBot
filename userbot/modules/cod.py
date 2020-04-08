455f1792938c547bf9862534dc4db698b5d2a27f
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-09 00:23:02 +0530

    Update and rename coronavirus.py to cod.py

--- userbot/modules/cod.py
+++ userbot/modules/cod.py
@@ -26,7 +26,7 @@ def get_country_data(country, world):
     
     
     CMD_HELP.update({
-    "cod19":
+    "cod":
     ".cod19 <country>\
 \nUsage: CoronaVirus LookUp for specified country if available."
 })
