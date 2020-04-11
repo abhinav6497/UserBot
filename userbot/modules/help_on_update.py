3c92fab0c73adac79d38eb31a487ff8b7b842030
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-11 16:12:15 +0530

    Update help_on_update.py

--- userbot/modules/help_on_update.py
+++ userbot/modules/help_on_update.py
@@ -26,9 +26,17 @@ async def usit(e):
         "\n[Ubuntu Linux Method](https://telegra.ph/How-to-keep-OUB-repo-updated-while-keeping-your-changes-through-Ubuntu-Terminal-method-04-01-2)"
         "\n[Special - Note](https://telegra.ph/Special-Note-11-02)")
     
+@register(outgoing=True, pattern="^.varoub$")
+async def var(m):
+    await m.edit(
+        f"Here's a list of VARS for {DEFAULTUSER} on **OpenUserBot**:\n"
+        "\n[HEROKU VARS](https://raw.githubusercontent.com/mkaraniya/OpenUserBot/sql-extended/bin/vars%20for%20oub.txt)")
+    
     
 CMD_HELP.update({
     "useitoub":
     ".useitoub\
-\nUsage: Provide links to update repo guides while you keep your changes on the floor."
+\nUsage: Provide links to update repo guides while you keep your changes on the floor.\
+\n.varoub\
+\nUsage: Provide vars to cross check for you."
 })
