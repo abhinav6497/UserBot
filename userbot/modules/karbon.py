38e9b297b3b1680bd954b5efdbf71382f0bd49b9
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-13 19:40:55 +0530

    Update karbon.py

--- userbot/modules/karbon.py
+++ userbot/modules/karbon.py
@@ -316,7 +316,7 @@ async def carbon_api(e):
     await e.delete()  # Deleting msg
     
 
-@register(outgoing=True, pattern="^.karbon1")
+@register(outgoing=True, pattern="^.karbon5")
 async def carbon_api(e):
     """ A Wrapper for carbon.now.sh """
     await e.edit("`Processing..`")
