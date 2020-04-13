081349c254ea232e4b92c7d41100df82ba2c095c
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-13 22:37:07 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -123,16 +123,16 @@ async def pipcheck(pip):
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit(
-                     "`i am alive My Mastor` \n"
-                     "`i can't die` \n"
-                     f"Telethon version: {version.__version__} \n"
-                     f"Python: {python_version()} \n"
+                     "`i am ᗩᒪᓰᐺᘿ My 𝕄𝕒𝕤𝕥𝕖𝕣` \n"
+                     "`𝘪 𝙘𝙖𝙣'𝙩 Ðïê` \n"
+                     f"тєℓєтнση νєяѕιση: {version.__version__} \n"
+                     f"P̳y̳t̳h̳o̳n̳ ̳v̳e̳r̳s̳i̳o̳n̳: {python_version()} \n"
                      f"------------------------------------ \n"
-                     f"Website: https://www.facebook.com/Teknoways \n"
-                     f"User: {DEFAULTUSER} \n"
-                     f"Maintainer: @Mayur_Karaniya \n"
-                     f"Admin: @Three_Cube_TeKnoways \n"
-                     f"Userbot: @testing_userbot "
+                     f"ᗯEᗷᔕITE: 𝖍𝖙𝖙𝖕𝖘://𝖜𝖜𝖜.𝖋𝖆𝖈𝖊𝖇𝖔𝖔𝖐.𝖈𝖔𝖒/𝕿𝖊𝖐𝖓𝖔𝖜𝖆𝖞𝖘 \n"
+                     f"U̴̧̡̫̤̦̇͆͛̿͑̈́̂̊̚͝s̷̡͓͎͘e̷̹̙̝̽̾͂ŕ̴̡̛̺̖̝̬̣͖͕̐̅͌͂͌̕:: {DEFAULTUSER} \n"
+                     f"Mαιɳƚαιɳҽɾ: @🄼🄰🅈🅄🅁_🄺🄰🅁🄰🄽🄸🅈🄰 \n"
+                     f"🅰🅳🅼🅸🅽: `@𝓣𝓱𝓻𝓮𝓮_𝓒𝓾𝓫𝓮_𝓣𝓮𝓚𝓷𝓸𝔀𝓪𝔂𝓼` \n"
+                     f"U҉s҉e҉r҉b҉o҉t҉: @ₜₑₛₜing_bₒₜ "
                      )    
 
 
