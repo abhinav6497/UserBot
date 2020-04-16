e707beff6fcdb6eb02f8e4162cb8a414969f282f
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-16 14:45:10 +0530

    Update useful.py

--- userbot/modules/useful.py
+++ userbot/modules/useful.py
@@ -1,7 +1,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-"""use cmd .load <plugin.py name>, .unload <plugin.py name>, send plugin <plugin.py name>, install <plugin.py name>."""
+# """use cmd .load <plugin.py name>, .unload <plugin.py name>, send plugin <plugin.py name>, install <plugin.py name>."""
 import asyncio
 import traceback
 import os
