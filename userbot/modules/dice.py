c414deadb29221fdfb18f4a40803641f8cce4c38
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-16 19:21:41 +0530

    Create dice.py

---
+++ userbot/modules/dice.py
@@ -0,0 +1,36 @@
+"""@RollADie
+Syntax: .dice"""
+#from telethon.tl.types import InputMediaDice
+import telethon
+#from uniborg.util import admin_cmd
+import telethon.tl.types
+from telethon.tl.types import *
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
+from userbot.events import register
+
+
+
+
+#@borg.on(admin_cmd(pattern="dice ?(.*)"))
+@register(outgoing=True, pattern="^.dice$")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.delete()
+    r = await event.reply(file=InputMediaDice())
+    if input_str:
+        try:
+            from telethon.tl.types import InputMediaDice
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice())
+        except:
+            pass
+            
+CMD_HELP.update({
+        "dice": 
+        ".dice reply_message. \
+          \n. "
+    })
