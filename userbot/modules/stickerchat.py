4048757030bb9061bcb6b3ce09cf59f990f46307
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-16 18:11:06 +0530

    Update stickerchat.py

--- userbot/modules/stickerchat.py
+++ userbot/modules/stickerchat.py
@@ -77,7 +77,7 @@ if 1 == 1:
         "channel": "Channel"
     }
 
-    config = dict({"api_token": os.environ.get("API_TOKEN"), 
+    config = API_TOKEN, 
                                           "api_url": "http://api.antiddos.systems",
                                           "username_colors": ["#fb6169", "#faa357", "#b48bf2", "#85de85",
                                                               "#62d4e3", "#65bdf3", "#ff5694"],
