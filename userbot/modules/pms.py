db3e2ca3a3c2746907e8a230d2cdcb28f2c81fbd
Abhinav Shinde <66168862+abhinav6497@users.noreply.github.com>
2020-06-19 11:52:28 +0530

    Update pms.py

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -251,7 +251,7 @@ async def blockpm(block):
     else:
         await block.client(BlockRequest(block.chat_id))
         aname = await block.client.get_entity(block.chat_id)
-        await block.edit("`You've been blocked 😡!`")
+        await block.edit("`My master thinks that you're useless person to discuss with.`\n\n`Hence, you've been blocked😡 :) !`")
         name0 = str(aname.first_name)
         uid = block.chat_id
 
