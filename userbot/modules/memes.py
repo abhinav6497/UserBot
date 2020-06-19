daa407f7b2f79707ec90cdfd7e30fdc134402cf2
Abhinav Shinde <66168862+abhinav6497@users.noreply.github.com>
2020-06-19 10:39:37 +0530

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -1502,10 +1502,10 @@ CMD_HELP.update({
 \nUsage: You retard !!\
 \n\n`.zal`\
 \nUsage: Invoke the feeling of chaos.\
-\n\nOem\
-\nUsage: Oeeeem\
-\n\nOof\
-\nUsage: Ooooof\
+\n\n`.oem`\
+\nUsage: oeeeem\
+\n\n`.oof`\
+\nUsage: ooooof\
 \n\n`.fp`\
 \nUsage: Facepalm :P\
 \n\n`.moon`\
