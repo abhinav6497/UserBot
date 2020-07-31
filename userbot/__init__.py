aeea98e089fee2bfdf4d45dd99692b3a7db3afaf
Abhinav Shinde <66168862+abhinav6497@users.noreply.github.com>
2020-07-31 15:07:22 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -314,8 +314,8 @@ AFKREASON = None
 
 
 def paginate_help(page_number, loaded_modules, prefix):
-    number_of_rows = 5
-    number_of_cols = 3
+    number_of_rows = 7
+    number_of_cols = 2
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
