582aee1f337ab4bcf5af028f9ff99ad638f03e89
Abhinav Shinde <6497.abhi@gmail.com>
2020-09-01 09:53:33 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -29,7 +29,7 @@ for module_name in ALL_MODULES:
 LOGS.info(f"You are running oub-remix [{BOT_VER}]")
 
 LOGS.info(
-    "Congratulations, your userbot is now running !! Test it by typing .start/.ping in any chat."
+    "Congratulations, your 尺 乇 从 工 乂 UserBot is now running !! Test it by typing .start/.ping in any chat."
     "\nIf you need assistance, head to https://t.me/PPE_Support")
     
     
