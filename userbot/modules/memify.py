b4081a5619438b36b47bb0003a9ef6a0468abf30
abhinav6497 <6497.abhi@gmail.com>
2020-09-27 10:45:24 +0530

    update
    Signed-off-by: abhinav6497 <6497.abhi@gmail.com>

--- userbot/modules/memify.py
+++ userbot/modules/memify.py
@@ -166,8 +166,3 @@ async def draw_meme_text(image_path, text):
     img.save(webp_file, "WebP")
     return webp_file
 
-CMD_HELP.update({
-    "mmf": ">`.mmf` texttop ; textbottom"
-    "\nUsage: Reply a sticker/image and send with cmd."
-    "\n\n>`.mem` your text"
-    "\nUsage: Reply a sticker/image/gif and get meme from bot"})
