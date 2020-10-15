d7e06413f17165522bf6fe5c73aa35c188b613f4
Abhinav <6497.abhi@gmail.com>
2020-10-15 15:05:21 +0000

    pylint: auto fixes
    Signed-off-by: abhinav6497 <6497.abhi@gmail.com>

--- userbot/modules/fake_gs.py
+++ userbot/modules/fake_gs.py
@@ -11,6 +11,7 @@ from wget import download
 
 from userbot.events import register
 
+
 @register(outgoing=True, pattern="^.fgs ((.*) ; (.*))")
 async def FakeGoogleSearch(event):
     """ Get a user-customised google search meme! """
@@ -20,20 +21,22 @@ async def FakeGoogleSearch(event):
         return
     if ";" in input_str:
         search, result = input_str.split(";", 1)
-    else: 
+    else:
         await event.edit("Invalid Input! Check help for more info!", del_in=5)
         return
-      
+
     await event.edit('Connecting to `https://www.google.com/` ...')
     await asyncio.sleep(2)
-    img='https://i.imgur.com/wNFr5X2.jpg'
-    r=download(img)
-    photo=Image.open(r)
-    drawing=ImageDraw.Draw(photo)
-    blue=(0,0,255)
-    black=(0,0,0)
-    font1=ImageFont.truetype("userbot/utils/styles/ProductSans-BoldItalic.ttf",20)
-    font2=ImageFont.truetype("userbot/utils/styles/ProductSans-Light.ttf",23)
+    img = 'https://i.imgur.com/wNFr5X2.jpg'
+    r = download(img)
+    photo = Image.open(r)
+    drawing = ImageDraw.Draw(photo)
+    blue = (0, 0, 255)
+    black = (0, 0, 0)
+    font1 = ImageFont.truetype(
+        "userbot/utils/styles/ProductSans-BoldItalic.ttf", 20)
+    font2 = ImageFont.truetype(
+        "userbot/utils/styles/ProductSans-Light.ttf", 23)
     drawing.text((450, 258), result, fill=blue, font=font1)
     drawing.text((270, 37), search, fill=black, font=font2)
     photo.save("downloads/test.jpg")
@@ -45,4 +48,3 @@ async def FakeGoogleSearch(event):
         'downloads/test.jpg',
         reply_to_message_id=reply_id)
     os.remove('downloads/test.jpg')
-
