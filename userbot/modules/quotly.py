dd9bcf2d4cc9b63786874dcd2dd54cafffbcb436
Abhinav Shinde <6497.abhi@gmail.com>
2020-10-19 17:23:08 +0530

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -1,69 +1,61 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+# Copyright (C) 2020 The Raphielscape Company LLC.
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-"""  Port From UniBorg to UserBot by @MoveAngel """
+# Port From UniBorg to UserBot by MoveAngel
 
-import os
+import telethon
 from asyncio.exceptions import TimeoutError
+
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
+from userbot import bot, CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern=r"^\.q(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.q")
 async def quotess(qotli):
     if qotli.fwd_from:
         return
     if not qotli.reply_to_msg_id:
-        await qotli.edit("```Reply to any user message.```")
-        return
+        return await qotli.edit("```Reply to any user message.```")
     reply_message = await qotli.get_reply_message()
     if not reply_message.text:
-        await qotli.edit("```Reply to text message```")
-        return
+        return await qotli.edit("```Reply to text message```")
     chat = "@QuotLyBot"
-    reply_message.sender
     if reply_message.sender.bot:
-        await qotli.edit("```Reply to actual users message.```")
-        return
+        return await qotli.edit("```Reply to actual users message.```")
+    await qotli.edit("```Making a Quote```")
     try:
-        await qotli.edit("`Processing..`")
         async with bot.conversation(chat) as conv:
             try:
                 response = conv.wait_event(
-                    events.NewMessage(incoming=True, from_users=1031952739)
-                )
+                    events.NewMessage(
+                        incoming=True,
+                        from_users=1031952739))
                 msg = await bot.forward_messages(chat, reply_message)
                 response = await response
+                """ - don't spam notif - """
                 await bot.send_read_acknowledge(conv.chat_id)
             except YouBlockedUserError:
-                await qotli.reply("```Please unblock @QuotLyBot and try again```")
-                return
+                return await qotli.reply("```Please unblock @QuotLyBot and try again```")
             if response.text.startswith("Hi!"):
-                await qotli.edit(
-                    "```Can you kindly disable your forward privacy settings for good?```"
-                )
+                await qotli.edit("```Can you kindly disable your forward privacy settings for good?```")
             else:
-                downloaded_file_name = await qotli.client.download_media(
-                    response.media, TEMP_DOWNLOAD_DIRECTORY
-                )
-                await qotli.client.send_file(
-                    qotli.chat_id, downloaded_file_name, reply_to=qotli.reply_to_msg_id
-                )
                 await qotli.delete()
+                await bot.forward_messages(qotli.chat_id, response.message)
                 await bot.send_read_acknowledge(qotli.chat_id)
-                await qotli.client.delete_messages(conv.chat_id, [msg.id, response.id])
-                os.remove(downloaded_file_name)
+                """ - cleanup chat after completed - """
+                await qotli.client.delete_messages(conv.chat_id,
+                                                   [msg.id, response.id])
     except TimeoutError:
-        await qotli.edit("`@QuotlyBot isnt responding`")
-        await qotli.client.delete_messages(conv.chat_id, [msg.id])
+        await qotli.edit()
 
 CMD_HELP.update({
     "quotly":
     "`.q`\
 \nUsage: Enhance ur text to sticker.\
 \n\n`.pch`\
-\nUsage: Better than quotly."})
+\nUsage: Better than quotly."
+})
